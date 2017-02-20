d01023f871e933b2ca4eec151c238e839480b7da
liuzunchang <liuzunchang@le.com>
2017-02-20 21:47:12 +0800

    fixbugs

--- smart_wifi.sh
+++ smart_wifi.sh
@@ -5,15 +5,16 @@ USER_KEY="RTjC-NdpBnwuQN1xE84pYSANSWSPCkVbpixp3XOY"
 LIGHT_IP="192.168.31.27"
 
 # Write your phone`s mac here
-ARRAY_PHONE_MACS=(28:A0:2B:2B:94:91 AC:29:3A:C4:DB:FA)
+#sh no support array
+ARRAY_PHONE_MACS="28:A0:2B:2B:94:91 AC:29:3A:C4:DB:FA"
 # Write your light num which you want control  here
 HUE_NUM='1'
 
 
-SUNSET_TIME='18'
-SUNRISE_TIME='8'
-WAKEUP_TIME='23'
-SLEEP_INTERVAL='3' 
+SUNSET_TIME=18
+WAKEUP_TIME=8
+SLEEP_TIME=23
+SLEEP_INTERVAL=3
 #-----------------------------------------
 
 openflag=0
@@ -40,17 +41,16 @@ find_mac()
 {
     iwinfo wl1 assoclist | grep WPA2 | awk '{print $1}' | while read MAC1 
     do
-        # echo $MAC1
-        for i in ${ARRAY_PHONE_MACS[@]}
+        echo $MAC1
+        set $ARRAY_PHONE_MACS
+        for i in $@
         do
-            if [ $MAC1 = '28:A0:2B:2B:94:91' ]; then
+            if [ $MAC1 = $i ]; then
+                echo "find it"
                 return 1
             fi
         done
 
-        if [ $MAC1 = '28:A0:2B:2B:94:91' ]; then
-            return 1
-        fi
     done
     # return 0
 }
@@ -70,7 +70,7 @@ light_flicke()
     sleep 0.5
     if [ $current_light_state = 'false' ]; then
         curl -X PUT -d '{"on":false}' http://$LIGHT_IP/api/$USER_KEY/lights/$HUE_NUM/state
-    if
+    fi
 
 }
 
@@ -93,25 +93,20 @@ do
 
     if [ $findmaster -eq 1 ] && [ $hour -ge $SUNSET_TIME ] && [ $openflag -eq 0 ];then
         curl -X PUT -d '{"on":true}' http://$LIGHT_IP/api/$USER_KEY/lights/$HUE_NUM/state
-        echo trn on
-        openflag=1 
+        echo turn on
+        openflag=1
     # find master left the router then light off 
-    elif [$findmaster -eq 0 ] && [ $current_light_state = 'true' ];then
+    elif [ $findmaster -eq 0 ] && [ $current_light_state = 'true' ];then
         curl -X PUT -d '{"on":false}' http://$LIGHT_IP/api/$USER_KEY/lights/$HUE_NUM/state
         echo trn off
         openflag=0
         closeflag=0
-
-    fi
-
     #  light off at sleep time 
-    elif [ $hour -eq SLEEP_TIME ] && [ $closeflag -eq 0 ];then
+    elif [ $hour -eq $SLEEP_TIME ] && [ $closeflag -eq 0 ];then
         closeflag=1
         curl -X PUT -d '{"on":false}' http://$LIGHT_IP/api/$USER_KEY/lights/$HUE_NUM/state
         echo trn off
     fi
-
-   
     if [ $hour -eq 8 ] ;then
         openflag=0
         closeflag=0
@@ -122,14 +117,16 @@ do
 
     #----------------------------------------------
      # light flicker at sharp time
-    if [ [ $minute -eq 0 ] && [ $flickerflag -eq 0 ] && [ $hour -ge WAKEUP_TIME ]; then
+    if [ $minute -eq 0 ] && [ $flickerflag -eq 0 ] && [ $hour -ge WAKEUP_TIME ]; then
         light_flicke
         flickerflag=1
     fi
+
     if [ $minute -eq 1 ] ;then
         flickerflag=0
     fi
 
-     sleep $SLEEP_INTERVAL
+
+    sleep $SLEEP_INTERVAL
 done
  
